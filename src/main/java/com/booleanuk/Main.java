2015420b1bc97d8a0d785a28ba98085fea421496
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-09 10:11:32 +0100

    setup

---
+++ src/main/java/com/booleanuk/Main.java
@@ -0,0 +1,29 @@
+package com.booleanuk;
+
+public class Main {
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int age = 24;
+        String firstName = "Vegard";
+        String lastName = "Vognstølen";
+        int day = 9;
+        int month = 1;
+        int year = 2024;
+        int agi = 11;
+        int str = 13;
+        int intell = 15;
+
+
+
+
+        System.out.println("My name is " + firstName + " " + lastName + " and my age is " + age + ". Furthermore, the date is 0" + day + "/" + month + "/" + year + ". My total of stats are currently: " + (intell + str + agi));
+
+
+
+    }
+}
